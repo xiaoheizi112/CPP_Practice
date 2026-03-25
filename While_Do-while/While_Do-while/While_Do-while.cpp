4015b3dd90749b58269bf2593fd154450becd5f5
xiaoheizi112 <1274077326@qq.com>
2026-03-25 13:27:59 +0800

    feat：添加while，dowhile语句

---
+++ While_Do-while/While_Do-while/While_Do-while.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    // while循环示例
+    int count = 1;
+    cout << "while循环输出: ";
+    while (count <= 5) {
+        cout << count << " ";
+        count++;
+    }
+    cout << endl;
+
+    // do-while循环示例
+    int number;
+    do {
+        cout << "请输入一个正数: ";
+        cin >> number;
+    } while (number <= 0);
+
+    cout << "你输入的正数是: " << number << endl;
+
+    return 0;
+}
