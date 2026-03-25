97e9aaccb5471b49fb0b006111478d15bcb0b70b
xiaoheizi112 <1274077326@qq.com>
2026-03-25 13:15:51 +0800

    feat：添加for语句

---
+++ For/For/For.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    // 基本的for循环
+    cout << "1-10的数字: ";
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // 计算1-100的和
+    int sum = 0;
+    for (int i = 1; i <= 100; i++) {
+        sum += i;
+    }
+    cout << "1-100的和为: " << sum << endl;
+
+    int number[] = {1 , 2 , 3 , 4 , 5};
+    for (int num : number)
+    {
+        cout << "num:" << num <<" ";
+    }
+
+
+    return 0;
+}
