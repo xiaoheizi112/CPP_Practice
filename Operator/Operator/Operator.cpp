cab3a343465243a209c5b6bf647a62ed879ffc38
xiaoheizi112 <1274077326@qq.com>
2026-03-25 12:45:58 +0800

    feat：添加运算符

---
+++ Operator/Operator/Operator.cpp
@@ -0,0 +1,28 @@
+﻿// Operator.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10, b = 5;
+    cout << "a+b = " << (a + b) << endl;
+    cout << "a-b = " << (a - b) << endl;
+    cout << "a*b = " << (a * b) << endl;
+    cout << "a/b = " << (a / b) << endl;
+    cout << "a%b = " << (a % b) << endl;
+
+    cout << "a > b: " << (a > b) << endl;
+    cout << "a == b: " << (a == b) << endl;
+
+    bool x = true, y = false;
+    cout << "x && y: " << (x && y) << endl;  
+    cout << "x || y: " << (x || y) << endl;  
+    cout << "!x: " << (!x) << endl;          
+
+    a += 5;  
+    cout << "a += 5 后: " << a << endl;
+    return 0;
+}
+
